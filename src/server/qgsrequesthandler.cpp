58a61a2f0765e20201a8cd7a37f48aae758c1695
andreassteffens <a.steffens@gds-team.de>
2023-12-14 12:20:58 +0100

    fixed typo

--- src/server/qgsrequesthandler.cpp
+++ src/server/qgsrequesthandler.cpp
@@ -225,7 +225,7 @@ void QgsRequestHandler::parseInput()
         bProcessAsQueryString = !doc.setContent( inputString, true, &errorMsg, &line, &column );
 
         // XXX Output error but continue processing request ?
-        QgsMessageLog::logMessage( QStringLiteral( "Warning: error parsing post data as XML: at line %1, column %2: %3. Assuming urlencoded query string sent in the post body  of request '%4'." )
+        QgsMessageLog::logMessage( QStringLiteral( "Warning: error parsing post data as XML: at line %1, column %2: %3. Assuming urlencoded query string sent in the post body of request '%4'." )
                                    .arg( line ).arg( column ).arg( errorMsg ).arg( mRequest.originalUrl().toString() ), QStringLiteral( "Server" ), Qgis::Warning );
       }
 
